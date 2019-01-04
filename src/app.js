f589070a8c952a92c81d0b838ead26fd29c25a0d
Taqin <taqin@megentics.com>
2019-01-04 14:38:47 +0800

    Init the MongoDB connection

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -20,7 +20,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use('/assets', express.static('public'));
 
 app.use('/', indexRouter);
